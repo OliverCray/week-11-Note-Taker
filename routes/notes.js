af317321760fea8336561f876bf30f962e58c26d
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-26 15:16:07 +0100

    Added GET route for a specific note

--- routes/notes.js
+++ routes/notes.js
@@ -11,3 +11,16 @@ notes.get('/', (req, res) => {
     console.info(`${req.method} request received for ${req.path}`)
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 })
+
+// GET route for a specific note
+tips.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.note_id === noteId)
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID')
+      })
+  })
