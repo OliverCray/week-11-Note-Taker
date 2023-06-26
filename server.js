754548a6b472728600cea4cf907394aeb35cf4b5
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-26 14:59:24 +0100

    Added GET routes for pages, added listen function

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', api)
+
+app.use(express.static('public'))
+
+// GET route for homepage, uses a widlcard route so that any route outside of those specified defaults to the homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+// GET route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
