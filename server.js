3008b539e1a92edbf9a351bedeeee8261bfe2961
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-26 15:05:07 +0100

    Changed homepage GET route from '*' to '/' Added additional wildcard route to direct users to the homepage

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const api = require('./routes/index')
+//const api = require('./routes/index')
 
 const PORT = process.env.PORT || 3001
 
@@ -9,18 +9,23 @@ const app = express()
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use('/api', api)
+//app.use('/api', api)
 
 app.use(express.static('public'))
 
-// GET route for homepage, uses a widlcard route so that any route outside of those specified defaults to the homepage
-app.get('*', (req, res) => {
+// GET route for homepage
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 // GET route for notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+// Wildcard route to direct users to homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 app.listen(PORT, () => {
